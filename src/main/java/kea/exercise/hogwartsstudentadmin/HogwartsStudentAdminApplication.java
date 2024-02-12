530c1fcf6ae4e1e8b3d67d6f6ede32626888e9ee
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-02-12 16:12:45 +0100

    initial commit

---
+++ src/main/java/kea/exercise/hogwartsstudentadmin/HogwartsStudentAdminApplication.java
@@ -0,0 +1,13 @@
+package kea.exercise.hogwartsstudentadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsStudentAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsStudentAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/exercise/hogwartsstudentadmin/HogwartsStudentAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.exercise.hogwartsstudentadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsStudentAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

204fbd88f4d75dff56d0cdeaf55780d1ca8ee5bc
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-02-13 22:31:24 +0100

    created InitData class

---
+++ src/main/java/kea/exercise/hogwartsstudentadmin/edu/hogwarts/config/InitData.java
@@ -0,0 +1,92 @@
+package kea.exercise.hogwartsstudentadmin.edu.hogwarts.config;
+
+import kea.exercise.hogwartsstudentadmin.edu.hogwarts.models.House;
+import kea.exercise.hogwartsstudentadmin.edu.hogwarts.models.Student;
+import kea.exercise.hogwartsstudentadmin.edu.hogwarts.models.Teacher;
+import kea.exercise.hogwartsstudentadmin.edu.hogwarts.models.EmpType;
+import kea.exercise.hogwartsstudentadmin.edu.hogwarts.repositories.HouseRepository;
+import kea.exercise.hogwartsstudentadmin.edu.hogwarts.repositories.StudentRepository;
+import kea.exercise.hogwartsstudentadmin.edu.hogwarts.repositories.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class InitData implements ApplicationRunner {
+
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private TeacherRepository teacherRepository;
+    @Autowired
+    private HouseRepository houseRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (houseRepository.count() == 0) {
+            House gryffindor = new House("Gryffindor", "Godric Gryffindor", new ArrayList<>(Arrays.asList("Scarlet", "Gold")));
+            House hufflepuff = new House("Hufflepuff", "Helga Hufflepuff", new ArrayList<>(Arrays.asList("Yellow", "Black")));
+            House ravenclaw = new House("Ravenclaw", "Rowena Ravenclaw", new ArrayList<>(Arrays.asList("Blue", "Bronze")));
+            House slytherin = new House("Slytherin", "Salazar Slytherin", new ArrayList<>(Arrays.asList("Green", "Silver")));
+
+            houseRepository.save(gryffindor);
+            houseRepository.save(hufflepuff);
+            houseRepository.save(ravenclaw);
+            houseRepository.save(slytherin);
+        }
+
+        if (studentRepository.count() == 0) {
+            List<Student> students = new ArrayList<>();
+            // get houses from the database
+            House gryffindor = houseRepository.findByName("Gryffindor");
+            House hufflepuff = houseRepository.findByName("Hufflepuff");
+            House ravenclaw = houseRepository.findByName("Ravenclaw");
+            House slytherin = houseRepository.findByName("Slytherin");
+
+            students.add(new Student("Harry", "James", "Potter", LocalDate.of(1980, Month.JULY, 31), gryffindor, false, 1991, 1998, false));
+            students.add(new Student("Hermione", "Jean", "Granger", LocalDate.of(1979, Month.SEPTEMBER, 19), gryffindor, false, 1991, 1998, false));
+            students.add(new Student("Ronald", "Bilius", "Weasley", LocalDate.of(1980, Month.MARCH, 1), gryffindor, false, 1991, 1998, false));
+            students.add(new Student("Draco", "Lucius", "Malfoy", LocalDate.of(1980, Month.JUNE, 5), slytherin, false, 1991, 1998, false));
+            students.add(new Student("Pansy", "Parkinson", LocalDate.of(1980, Month.JULY, 4), slytherin, false, 1991, 1998, false));
+            students.add(new Student("Vincent", "Crabbe", LocalDate.of(1980, Month.JANUARY, 1), slytherin, false, 1991, 1998, false));
+            students.add(new Student("Padma", "Patil", LocalDate.of(1980, Month.APRIL, 1), ravenclaw, false, 1991, 1998, false));
+            students.add(new Student("Terry", "Boot", LocalDate.of(1980, Month.MAY, 1), ravenclaw, false, 1991, 1998, false));
+            students.add(new Student("Michael", "Corner", LocalDate.of(1980, Month.JUNE, 1), ravenclaw, false, 1991, 1998, false));
+            students.add(new Student("Justin", "Finch-Fletchley", LocalDate.of(1980, Month.JULY, 1), hufflepuff, false, 1991, 1998, false));
+            students.add(new Student("Hannah", "Abbott", LocalDate.of(1980, Month.AUGUST, 1), hufflepuff, false, 1991, 1998, false));
+            students.add(new Student("Susan", "Bones", LocalDate.of(1980, Month.SEPTEMBER, 1), hufflepuff, false, 1991, 1998, false));
+
+            studentRepository.saveAll(students);
+        }
+
+        if (teacherRepository.count() == 0) {
+            List<Teacher> teachers = new ArrayList<>();
+
+            House gryffindor = houseRepository.findByName("Gryffindor");
+            House hufflepuff = houseRepository.findByName("Hufflepuff");
+            House ravenclaw = houseRepository.findByName("Ravenclaw");
+            House slytherin = houseRepository.findByName("Slytherin");
+
+            EmpType tenured = EmpType.TENURED;
+            EmpType temporary = EmpType.TEMPORARY;
+
+            teachers.add(new Teacher("Minerva", "McGonagall", LocalDate.of(1935, Month.OCTOBER, 4), gryffindor, true, tenured, LocalDate.of(1960, Month.SEPTEMBER, 1), null));
+            teachers.add(new Teacher("Pomona", "Sprout", LocalDate.of(1931, Month.MAY, 15), hufflepuff, true, tenured, LocalDate.of(1975, Month.SEPTEMBER, 1), null));
+            teachers.add(new Teacher("Filius", "Flitwick", LocalDate.of(1935, Month.OCTOBER, 17), ravenclaw, true, tenured, LocalDate.of(1975, Month.SEPTEMBER, 1), null));
+            teachers.add(new Teacher("Severus", "Snape", LocalDate.of(1960, Month.JANUARY, 9), slytherin, true, tenured, LocalDate.of(1981, Month.SEPTEMBER, 1), null));
+            teachers.add(new Teacher("Horace", "Slughorn", LocalDate.of(1882, Month.APRIL, 28), slytherin, false, temporary, LocalDate.of(1996, Month.SEPTEMBER, 1), LocalDate.of(1997, Month.JUNE, 30)));
+            teachers.add(new Teacher("Alastor", "Moody", LocalDate.of(1960, Month.JANUARY, 1), gryffindor, false, temporary, LocalDate.of(1994, Month.SEPTEMBER, 1), LocalDate.of(1997, Month.JUNE, 30)));
+            teachers.add(new Teacher("Remus", "Lupin", LocalDate.of(1960, Month.MARCH, 10), gryffindor, false, temporary, LocalDate.of(1993, Month.SEPTEMBER, 1), LocalDate.of(1997, Month.JUNE, 30)));
+
+
+            teacherRepository.saveAll(teachers);
+        }
+    }
+}

510b6e3266fd89197168e8d47fa770d72aa8d354
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-02-24 21:03:28 +0100

    added findByFullName

--- src/main/java/kea/exercise/hogwartsstudentadmin/edu/hogwarts/repository/StudentRepository.java
+++ src/main/java/kea/exercise/hogwartsstudentadmin/edu/hogwarts/repository/StudentRepository.java
@@ -1,7 +1,16 @@
 package kea.exercise.hogwartsstudentadmin.edu.hogwarts.repository;
 
 import kea.exercise.hogwartsstudentadmin.edu.hogwarts.model.Student;
+import kea.exercise.hogwartsstudentadmin.edu.hogwarts.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s FROM Student s WHERE " +
+            "(s.middleName IS NULL OR s.middleName = '') AND CONCAT(s.firstName, ' ', s.lastName) = :fullName " +
+            "OR CONCAT(s.firstName, ' ', s.middleName, ' ', s.lastName) = :fullName")
+    Optional<Student> findByFullName(@Param("fullName") String fullName);
 }
--- src/main/java/kea/exercise/hogwartsstudentadmin/edu/hogwarts/repository/TeacherRepository.java
+++ src/main/java/kea/exercise/hogwartsstudentadmin/edu/hogwarts/repository/TeacherRepository.java
@@ -2,6 +2,14 @@ package kea.exercise.hogwartsstudentadmin.edu.hogwarts.repository;
 
 import kea.exercise.hogwartsstudentadmin.edu.hogwarts.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    @Query("SELECT t FROM Teacher t WHERE " +
+            "(t.middleName IS NULL OR t.middleName = '') AND CONCAT(t.firstName, ' ', t.lastName) = :fullName " +
+            "OR CONCAT(t.firstName, ' ', t.middleName, ' ', t.lastName) = :fullName")
+    Optional<Teacher> findByFullName(@Param("fullName") String fullName);
 }

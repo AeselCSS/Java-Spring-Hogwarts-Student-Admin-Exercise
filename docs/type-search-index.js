135fb5343c82eead49612ebf3f9348c4511ef2ba
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-02-25 01:37:35 +0100

    java docs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"kea.exercise.hogwartsstudentadmin.edu.hogwarts.service","l":"CourseServiceImpl"}];updateSearchResults();
